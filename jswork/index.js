fbe7fc4ceaee3bf14abf34cdc0072606a280aead
kshitizsinghal13 <k3040861@gmail.com>
2024-10-18 14:12:39 +0530

    addjs

---
+++ jswork/index.js
@@ -0,0 +1,43 @@
+// // console.log("hello love");
+// // let a=12
+// // console.log(a)
+// // if(a>10){
+// //     let b=30;
+// //     console.log("hi",a);
+// // }
+// // console.log(a)
+
+
+
+
+
+
+
+
+
+
+
+
+// let a='12';
+// let b=12;
+// if(a==b){
+//     console.log("cheese maggie");
+// }
+// else{
+//     console.log("loki ki sabji")
+// }
+// let d=function(str=" matar k saath khayega hastar"){
+//     console.log("aalu ki paani vaali sabji"+str);
+// }
+// d();
+
+
+
+// function sum(a,b,c=12){
+//     return a+b+c
+// }
+// let total=sum(50,19)
+// console.log("hastar daadi k saath karega "+ total)
+
+console.dir(document)
+
